8be9f6ec239298b2cc33fb90aab30610d17f435e
Geon2 <121271035+geon-2@users.noreply.github.com>
2024-04-25 18:45:09 +0900

    Update NavigationBar.jsx

--- Chapter 3/Movie Poster/src/components/NavigationBar.jsx
+++ Chapter 3/Movie Poster/src/components/NavigationBar.jsx
@@ -8,55 +8,55 @@ const FlexList = `
 `;
 
 const Header = styled.header`
-    ${FlexList}
+    	${FlexList}
 	width: 100%;
 	height: 5rem;
-    background: #171B39;
-    box-sizing: border-box;
-    padding: 0 2rem;
+	background: #171B39;
+	box-sizing: border-box;
+	padding: 0 2rem;
 `;
 
 const Nav = styled.nav`
 	${FlexList};
-    width: 40rem;
+	width: 40rem;
 `;
 
 const Home = styled(Link)`
-    color: #fff;
-    font-size: 2rem;
+	color: #fff;
+	font-size: 2rem;
 `
 
 const StyledLink = styled(Link).attrs(props => ({
-    isActive: props.isActive
+	isActive: props.isActive
 }))`
-    font-size: 1.5rem;
-    font-weight: ${({ isActive }) => isActive ? '600' : '400'};
-    color: ${({ isActive }) => isActive ? 'yellow' : '#fff'};
-    &:hover {
-        font-weight: 800;
-    }
+	font-size: 1.5rem;
+	font-weight: ${({ isActive }) => isActive ? '600' : '400'};
+	color: ${({ isActive }) => isActive ? 'yellow' : '#fff'};
+	&:hover {
+	font-weight: 800;
+	}
 `
 
 function NavigationBar() {
-    const location = useLocation();
-    
-    const isActive = (path) => {
-        return location.pathname === path;
-    }
-
-    const linkList = [
-        { url: '/signup', text: '회원가입' },
-        { url: '/list/popular', text: 'Popular' },
-        { url: '/list/now_playing', text: 'Now Playing' },
-        { url: '/list/top_rated', text: 'Top Rated' },
-        { url: '/list/upcoming', text: 'Upcoming' }
-    ]
-
+	const location = useLocation();
+	
+	const isActive = (path) => {
+		return location.pathname === path;
+	}
+	
+	const linkList = [
+		{ url: '/signup', text: '회원가입' },
+		{ url: '/list/popular', text: 'Popular' },
+		{ url: '/list/now_playing', text: 'Now Playing' },
+		{ url: '/list/top_rated', text: 'Top Rated' },
+		{ url: '/list/upcoming', text: 'Upcoming' }
+	]
+	
 	return (
 		<Header>
-            <Home>UMC Movie</Home>
+			<Home>UMC Movie</Home>
 			<Nav>
-                {linkList.map((item, index) => <StyledLink key={index} to={item.url} isActive={isActive(item.url)}>{item.text}</StyledLink> )}
+				{linkList.map((item, index) => <StyledLink key={index} to={item.url} isActive={isActive(item.url)}>{item.text}</StyledLink> )}
 			</Nav>
 		</Header>
 	);
