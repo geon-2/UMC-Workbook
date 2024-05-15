164795167cedf171953580df69d01f214061fc51
geon-2 <gunle0416@gmail.com>
2024-05-15 23:40:32 +0900

    DEL .DS_Store

--- Movie Poster/src/routes/signup-page.jsx
+++ Movie Poster/src/routes/signup-page.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { useState, useEffect } from 'react';
-import Body from '../components/body';
+import Body from '../components/Body';
 
 const Form = styled.form`
     diplay: flex;
